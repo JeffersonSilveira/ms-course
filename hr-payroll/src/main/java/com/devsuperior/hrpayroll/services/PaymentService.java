9e866cadca30140b5cc8fe94bd7eb7e380473b84
JeffersonSilveira <jeffersonsilveira30@gmail.com>
2020-12-09 18:16:23 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,8 +1,5 @@
 package com.devsuperior.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +9,7 @@ import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
-	
+
 	@Autowired
 	private WorkerFeignClient workerFeignClient;
 
